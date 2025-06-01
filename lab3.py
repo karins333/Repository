ab02a81c96d854d72b64391e970e066b3feed9e5
karins333 <bergovinakarina@gmail.com>
2025-06-02 02:40:37 +0300

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -10,10 +10,12 @@ df['species'] = iris.target
 df['species'] = df['species'].map({0: 'setosa', 1: 'versicolor', 2: 'virginica'})
 
 plt.figure(figsize=(10, 6))
-sns.scatterplot(data=df, x='sepal length (cm)', y='sepal width (cm)', hue='species', palette='Set1')
+sns.scatterplot(data=df, x='sepal length (cm)', y='sepal width (cm)', hue='species', palette='Set1', s=80, alpha=0.8)
 
-plt.title('Диаграмма рассеяния')
-plt.xlabel('Длина чашелистика (см)')
-plt.ylabel('Ширина чашелистика (см)')
-plt.legend(title='Вид')
+plt.title('Зависимость длины и ширины чашелистика у разных видов ирисов', pad=20)
+plt.xlabel('Длина чашелистика (см)', fontsize=12)
+plt.ylabel('Ширина чашелистика (см)', fontsize=12)
+plt.legend(title='Вид ириса', bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.grid(True, linestyle='--', alpha=0.3)
+plt.tight_layout()
 plt.show()
